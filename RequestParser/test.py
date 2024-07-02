8e6d918341aeb3268986e3dbe3c0eaa3746ec00a
Nikita Parmenov <parmenov.nik44@gmail.com>
2024-07-02 23:14:14 +0300

    fix error code

--- RequestParser/test.py
+++ RequestParser/test.py
@@ -1,12 +1,12 @@
 import csv
+import os
 from telethon import TelegramClient
-from config import api_id, api_hash, keys
+from config import keys
 from datetime import date, timedelta
 
 current_date = date.today()
 days_in_year = 365
 date_one_year_ago = current_date - timedelta(days=days_in_year)
-
 
 # Создаем CSV файл и открываем его для записи
 with open('messages.csv', 'w', newline='', encoding='utf-8') as csvfile:
@@ -16,7 +16,7 @@ with open('messages.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csv_writer.writerow(['text'])
 
     # Создаем клиент Телеграмма
-    with TelegramClient("userBot", api_id=api_id, api_hash=api_hash, device_model="iPhone 55 Pro",
+    with TelegramClient("userBot", api_id=os.getenv("API_ID"), api_hash=os.getenv("API_HASH"), device_model="iPhone 55 Pro",
                         system_version="IOS 100.1") as client:
         all_messages = client.iter_messages('https://t.me/infobizz1', reverse=True, offset_date=date_one_year_ago)
         a = 0
