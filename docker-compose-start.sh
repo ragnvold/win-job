ec89420af4bc20c294d4f54e13e11e90a2df470c
Nikita Parmenov <parmenov.nik44@gmail.com>
2024-07-31 14:50:04 +0300

    add switch between test and prod configs

---
+++ docker-compose-start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Проверяем, передан ли аргумент
+if [ -z "$1" ]; then
+  echo "Usage: $0 {prod|test}"
+  exit 1
+fi
+
+# Устанавливаем переменную WINJOB_ENV_FILE в зависимости от переданного аргумента
+case "$1" in
+  prod)
+    WINJOB_ENV_FILE=".env.prod"
+    ;;
+  test)
+    WINJOB_ENV_FILE=".env.test"
+    ;;
+  *)
+    echo "Unknown environment: $1"
+    echo "Usage: $0 {prod|test}"
+    exit 1
+    ;;
+esac
+
+# Экспортируем переменную WINJOB_ENV_FILE
+export WINJOB_ENV_FILE
+
+# Запускаем docker-compose с нужным файлом окружения
+sudo docker-compose up -d --build --no-deps --remove-orphans
